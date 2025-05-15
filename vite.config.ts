078c3de954bb3252bc1479d90b47ce2c5a14fd05
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-15 14:54:51 -0300

    configuraciones - se agregan los types de node y nuevas rutas dinamicas

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Obtener el directorio actual en formato ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
@@ -9,10 +14,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@img': path.resolve(__dirname, './src/assets/img'),
+      '@videos': path.resolve(__dirname, './src/assets/videos'),
       // '@api': path.resolve(__dirname, './src/api'),
       '@components': path.resolve(__dirname, './src/components'),
       '@modules': path.resolve(__dirname, './src/modules'),
       '@shared': path.resolve(__dirname, './src/shared'),
+      '@theme': path.resolve(__dirname, './src/theme'),
     },
   },
 })
