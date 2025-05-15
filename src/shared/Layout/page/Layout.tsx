58f64cd416b6b4ff1c556da225f88d6207f02efb
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-15 14:58:21 -0300

    md layout.tsx - se agrega la etiqueta main a todo lo que venga por rutas

--- src/shared/Layout/page/Layout.tsx
+++ src/shared/Layout/page/Layout.tsx
@@ -1,13 +1,20 @@
+// src/shared/Layout/page/Layout.tsx
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import React from "react";
+import { Box } from "@mui/material";
 
 export const Layout: React.FC = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Box 
+      component={"main"}
+      sx={{ flexGrow: 1 }}
+      >
+        <Outlet />
+      </Box>
       <Footer />
     </>
   );
