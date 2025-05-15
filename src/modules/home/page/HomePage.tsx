bf72b30c825f44f72a6b0ced8045be0ccf41a318
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-15 17:22:41 -0300

    md HomePage - se agrega la seccionde services a la home

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,12 +1,14 @@
 // src/modules/home/page/HomePage.tsx
 import React from "react";
 import { Hero } from "../components/Hero";
+import { Services } from "../components/Services";
 
 export const HomePage: React.FC = () => {
  
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 };
