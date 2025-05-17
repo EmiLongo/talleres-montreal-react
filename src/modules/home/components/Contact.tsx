1ff1730e4a6435a44a9545c9a59d926ce5e30635
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-16 22:36:36 -0300

    md contact - se agregan los componentes del mapa y formulario

--- src/modules/home/components/Contact.tsx
+++ src/modules/home/components/Contact.tsx
@@ -1,161 +1,96 @@
-import React from 'react';
-import { Box, TextField, Button, useTheme } from '@mui/material';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { InputError, SubtitleXL, Text2, TitleH1, TitleXL } from '@/theme/textStyles';
-
-const validationSchema = Yup.object({
-  name: Yup.string().required('Requerido'),
-  email: Yup.string().email('Correo inválido').required('Requerido'),
-  phone: Yup.string().required('Requerido'),
-  company: Yup.string(),
-  message: Yup.string().required('Por favor, escriba un mensaje'),
-});
+// src/modules/home/components/Contact.tsx
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { SubtitleXL, Text2, TitleH1, TitleXL } from "@/theme/textStyles";
+import { GoogleMapCard } from "./GoogleMap";
+import { ContactForm } from "./ContactForm";
 
 export const Contact: React.FC = () => {
   const theme = useTheme();
   const { palette } = theme;
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      phone: '',
-      company: '',
-      message: '',
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      console.log('Formulario enviado:', values);
-      // Aquí podrías enviar los datos a una API
-    },
-  });
-
   return (
-    <Box id="contacto" sx={{ py: 10, backgroundColor: 'primary.main', color: 'white' }}>
-      <Box sx={{ maxWidth: 1200, mx: 'auto', px: 2 }}>
-        {/* Título de sección */}
-        <Box sx={{ textAlign: 'center', mb: 6 }}>
-          <TitleXL
-            id="contact"
-            sx={{
-              color: palette.primary[100],
-              marginBottom: "1rem",
-            }}
-          >
-            Contacto
-          </TitleXL>
-
-          <SubtitleXL sx={{ color: palette.primary[100], }}>
-            Estamos a su disposición para brindarle el mejor servicio.
+    <Box
+      id="contacto"
+      sx={{ 
+        paddingY: "4rem", 
+        paddingX: { xs: "2rem", md: "4rem" },
+        backgroundColor: "primary.main", 
+        color: "white" 
+      }}
+    >
+      <TitleXL
+        id="contact"
+        sx={{
+          color: palette.primary[100],
+          marginBottom: "1rem",
+          textAlign: "center",
+        }}
+      >
+        Contacto
+      </TitleXL>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box
+          sx={{
+            textAlign: "center",
+            mb: 6,
+            maxWidth: { xs: "100vw", md: "50vw" },
+          }}
+        >
+          <SubtitleXL sx={{ color: palette.primary[100] }}>
+            ¿Tienes alguna pregunta específica? Contáctanos, estamos para
+            ayudarte.
           </SubtitleXL>
         </Box>
-
-        {/* Contenido: info + formulario */}
+        <ContactForm
+          sx={{
+            flex: 1,
+            maxWidth: { xs: "100vw", md: "50vw" },
+            width: { xs: "300px", sm: "400px", md: "600px" },
+          }}
+        />
+      </Box>
+      {/* Contenido: info + formulario */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {/* Información de contacto */}
+        <GoogleMapCard
+          srcMap="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.9621951281188!2d-60.7383915333192!3d-32.76673763876167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b65b1be6d77371%3A0xddf8f20133c12343!2sTalleres%20Montreal%20SRL!5e0!3m2!1ses!2sar!4v1747424640892!5m2!1ses!2sar"
+          titleMap="Ubicación de Talleres Montreal SRL"
+          titleCard="Nuestra ubicación"
+          sx={{
+            flex: 1,
+            mr: 2,
+            maxWidth: { md: "50%" },
+            aspectRatio: { xs: "1", md: "16/9" },
+          }}
+        />
         <Box>
-          {/* Información de contacto */}
-          <Box>
-            <TitleH1 sx={{ color: palette.primary[100], }}>
-              Información de Contacto
-            </TitleH1>
-            <Box sx={{ mb: 2, }}>
-              <Text2 sx={{color: palette.primary[100]}}>📞 341-3389977</Text2>
-              <Text2 sx={{color: palette.primary[100]}}>📞 3476-619576</Text2>
-              <Text2 sx={{color: palette.primary[100]}}>📞 3476-224447</Text2>
-              <Text2 sx={{color: palette.primary[100]}}>✉️ talleresmontrealsrl@gmail.com</Text2>
-              <Text2 sx={{color: palette.primary[100]}}>📍 José Márquez 856, San Lorenzo, Santa Fe</Text2>
-            </Box>
-          </Box>
-
-          {/* Formulario de contacto */}
-          <Box>
-            <TitleH1 sx={{ color: palette.primary[100], }}>
-              Envíenos un Mensaje
-            </TitleH1>
-            <Box component="form" onSubmit={formik.handleSubmit} noValidate>
-              <TextField
-                fullWidth
-                label="Nombre y Apellido"
-                variant="filled"
-                id="name"
-                name="name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.name && Boolean(formik.errors.name)}
-                // helperText={formik.touched.name && formik.errors.name}
-                sx={{backgroundColor: 'white', borderRadius: 1 }}
-              />
-              <InputError sx={{ mb: 2 }}>
-                {formik.touched.name && formik.errors.name}
-              </InputError>
-              <TextField
-                fullWidth
-                label="Correo Electrónico"
-                variant="filled"
-                id="email"
-                name="email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-                sx={{ backgroundColor: 'white', borderRadius: 1 }}
-              />
-              <InputError sx={{ mb: 2 }}>
-                {formik.touched.email && formik.errors.email}
-              </InputError>
-              <TextField
-                fullWidth
-                label="Teléfono"
-                variant="filled"
-                id="phone"
-                name="phone"
-                value={formik.values.phone}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.phone && Boolean(formik.errors.phone)}
-                sx={{ backgroundColor: 'white', borderRadius: 1 }}
-              />
-              <InputError sx={{ mb: 2 }}>
-                {formik.touched.phone && formik.errors.phone}
-              </InputError>
-              <TextField
-                fullWidth
-                label="Empresa"
-                variant="filled"
-                id="company"
-                name="company"
-                value={formik.values.company}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.company && Boolean(formik.errors.company)}
-                sx={{ backgroundColor: 'white', borderRadius: 1 }}
-              />
-              <InputError sx={{ mb: 2 }}>
-                {formik.touched.company && formik.errors.company}
-              </InputError>
-              <TextField
-                fullWidth
-                label="Mensaje"
-                multiline
-                rows={6}
-                variant="filled"
-                id="message"
-                name="message"
-                value={formik.values.message}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.message && Boolean(formik.errors.message)}
-                sx={{ backgroundColor: 'white', borderRadius: 1 }}
-              />
-              <InputError sx={{ mb: 2 }}>
-                {formik.touched.message && formik.errors.message}
-              </InputError>
-              <Button type="submit" variant="contained" color="secondary">
-                Enviar Mensaje
-              </Button>
-            </Box>
+          <TitleH1 sx={{ color: palette.primary[100] }}>
+            Información de Contacto
+          </TitleH1>
+          <Box sx={{ mb: 2 }}>
+            <Text2 sx={{ color: palette.primary[100] }}>📞 341-3389977</Text2>
+            <Text2 sx={{ color: palette.primary[100] }}>📞 3476-619576</Text2>
+            <Text2 sx={{ color: palette.primary[100] }}>📞 3476-224447</Text2>
+            <Text2 sx={{ color: palette.primary[100] }}>
+              ✉️ talleresmontrealsrl@gmail.com
+            </Text2>
+            <Text2 sx={{ color: palette.primary[100] }}>
+              📍 José Márquez 856, San Lorenzo, Santa Fe
+            </Text2>
           </Box>
         </Box>
       </Box>
