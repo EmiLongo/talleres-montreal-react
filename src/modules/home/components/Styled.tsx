0e1592ca23193972fd7045a9969ba1b9dca8688c
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-23 23:19:59 -0300

    add componentes para titulos de secciones

---
+++ src/modules/home/components/Styled.tsx
@@ -0,0 +1,37 @@
+import { Text1, Title1 } from "@/theme/textStyles";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode
+  sx?: object
+  id?: string
+}
+export const SectionTitle: React.FC<Props> = ({ children, sx, id }) => {
+  return (
+    <Title1
+      id={id}
+      sx={{
+        marginBottom: "1rem",
+        textAlign: "center",
+      ...sx }}>
+      { children }
+    </Title1>
+  )
+};
+
+export const SectionSubTitle: React.FC<Props> = ({ children, sx, id }) => {
+  return (
+    <Text1
+    id={id}
+    sx={{
+      textAlign: "center", 
+
+      paddingX: {xs: "2rem", md: "unset"}, 
+      maxWidth: {xs: "unset", md: "600px"}, 
+      textWrap: "balance",
+      ...sx}}
+    >
+      { children }
+    </Text1>
+  )
+}
