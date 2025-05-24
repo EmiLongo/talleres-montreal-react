cb10b7cc742645ccc15236e7b844e9856f49a009
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-23 23:13:41 -0300

    md textstyles - se acomodan algunos tamanios con el nuevo disenio

--- src/theme/textStyles.tsx
+++ src/theme/textStyles.tsx
@@ -16,26 +16,37 @@ const responsiveFontSize = (fontSizes: { xl?: string; lg?: string; md?: string; 
 export const TitleXL = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultTitle(theme),
   ...responsiveFontSize({ xl: '2.25rem', lg: '2.05rem', md: '1.95rem', sm: '1.85rem', xs: '1.85rem' })(theme), // 36px
+  textTransform: "uppercase",
 }));
 
 export const Title1 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultTitle(theme),
-  ...responsiveFontSize({ xl: '1.5rem', lg: '1.375rem', md: '1.325rem', sm: '1.3rem', xs: '1.3rem' })(theme), // 24px
+  ...responsiveFontSize({ xl: '1.75rem', lg: '1.59rem', md: '1.52rem', sm: '1.44rem', xs: '1.44rem' })(theme), // 24px
   textWrap: "wrap",
+  textTransform: "uppercase",
 }));
 
 export const Title2 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultTitle(theme),
-  ...responsiveFontSize({ xl: '1.25rem', lg: '1.15rem', md: '1.1rem', sm: '1.05rem', xs: '1.05rem' })(theme), // 20px
+  ...responsiveFontSize({ xl: '1.5rem', lg: '1.375rem', md: '1.325rem', sm: '1.3rem', xs: '1.3rem' })(theme), // 20px
   textWrap: "wrap",
+  textTransform: "uppercase",
 }));
 
 export const Title3 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultTitle(theme),
-  ...responsiveFontSize({ xl: '1.02rem', lg: '0.9375rem', md: '0.9rem', sm: '0.85rem', xs: '0.85rem' })(theme), // 16px
+  ...responsiveFontSize({ xl: '1.25rem', lg: '1.15rem', md: '1.1rem', sm: '1.05rem', xs: '1.05rem' })(theme), // 16px
   fontWeight: 'normal',
   textWrap: "wrap",
+  textTransform: "uppercase",
 }));
+// export const Title3 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
+//   ...defaultTitle(theme),
+//   ...responsiveFontSize({ xl: '1.02rem', lg: '0.9375rem', md: '0.9rem', sm: '0.85rem', xs: '0.85rem' })(theme), // 16px
+//   fontWeight: 'normal',
+//   textWrap: "wrap",
+//   textTransform: "uppercase",
+// }));
 
 // CTA
 export const CallToAction = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
@@ -48,7 +59,7 @@ export const CallToAction = styled((props: TypographyProps) => <Typography {...p
 
 export const SubtitleXL = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultParagraph(theme),
-  ...responsiveFontSize({ xl: "1.5rem", lg: "1.375rem", md: "1.32rem", sm: "1.27rem", xs: "1.27rem"})(theme), // 18px
+  ...responsiveFontSize({ xl: "1.5rem", lg: "1.375rem", md: "1.32rem", sm: "1.27rem", xs: "1.27rem"})(theme), // 24px
   textWrap: "wrap",
 }));
 
@@ -74,11 +85,17 @@ export const ButtonMini = styled((props: TypographyProps) => <Typography {...pro
 // PARAGRAPHS
 export const Text1 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultParagraph(theme),
-  ...responsiveFontSize({ xs: '1.1rem', md: '1.1rem', lg: '1.2rem', xl: '1.3rem' })(theme),
+  ...responsiveFontSize({ xs: '1.15rem', md: '1.21rem', lg: '1.28rem', xl: '1.4rem' })(theme),
   textWrap: "wrap",
 }));
 
 export const Text2 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
+  ...defaultParagraph(theme),
+  ...responsiveFontSize({ xs: '1.1rem', md: '1.1rem', lg: '1.2rem', xl: '1.3rem' })(theme),
+  textWrap: "wrap",
+}));
+
+export const Text3 = styled((props: TypographyProps) => <Typography {...props} />)(({ theme }) => ({
   ...defaultParagraph(theme),
   ...responsiveFontSize({ xs: '0.8rem', md: '1rem', lg: '1.1rem', xl: '1.2rem' })(theme),
   textWrap: "wrap",
