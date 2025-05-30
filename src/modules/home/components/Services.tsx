4e7f926bc9c72c92dd28bd2ded50d7193e65df63
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-05-30 12:10:02 -0300

    md services - se cambia el icono de favorito, se agrega profundidad al ciculo de la img, se componetiza fuera para separar favorito de no favorito, se cambian los textos, se acomoda el responsive

--- src/modules/home/components/Services.tsx
+++ src/modules/home/components/Services.tsx
@@ -1,5 +1,5 @@
 // src/modules/home/components/Services.tsx
-import { Box, useTheme } from "@mui/material";
+import { Box, keyframes, Paper, useTheme } from "@mui/material";
 import React from "react";
 import { Text1, Title1 } from "@theme/textStyles";
 import { SectionSubTitle, SectionTitle } from "./Styled";
@@ -8,6 +8,7 @@ import mecanizadoPrecision from "@img/services/mecanizado_precision.gif";
 import modeladoComplejo from "@img/services/modelado_complejo.gif";
 import reparacionFabricacion from "@img/services/reparacion_fabricacion.gif";
 import transporte from "@img/services/transporte.gif";
+import elaboracionPlanos from "@img/services/elaboracion_planos.gif";
 import favourite from "@img/services/favourite.svg";
 
 const services = [
@@ -30,21 +31,97 @@ const services = [
     isFavourite: true,
   },
   {
-    title: "Relevamiento y Elaboración de Planos",
-    description: "Registramos medidas, estructuras y condiciones existentes y elaboramos planos técnicos detallados.",
-    icon: mecanizadoPrecision,
-  },
-  {
     title: "Mecanizado de Precisión",
     description: "Mecanizado de piezas de alta precisión y calidad en amplia variedad de materiales.",
     icon: mecanizadoPrecision,
+    isFavourite: false,
+  },
+  {
+    title: "Relevamiento y Elaboración de Planos",
+    description: "Registramos medidas, estructuras y condiciones existentes y elaboramos planos técnicos detallados.",
+    icon: elaboracionPlanos,
+    isFavourite: false,
   },
   {
     title: "Reparación y Fabricación",
     description: "Reparación de equipos y fabricación de componentes industriales según sus necesidades.",
     icon: reparacionFabricacion,
+    isFavourite: false,
   },
 ]
+
+const rotateScaleUp = keyframes`
+  0% {
+    transform: scale(1) rotate3d(-1, 1, 0, 0deg);
+  }
+  10% {
+    transform: scale(1.5) rotate3d(-1, 1, 0, 180deg);
+  }
+  20% {
+    transform: scale(1) rotate3d(-1, 1, 0, 360deg);
+  }
+  100% {
+    transform: scale(1) rotateZ(360deg);
+  }
+`;
+
+const ServiceCard: React.FC<{ service: typeof services[number] }> = ({ service }) => {
+  const theme = useTheme();
+  const { palette } = theme;
+  return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "1rem",
+          width: {xs: "100%", sm: "45%", md: "27%"},
+          maxWidth: {xs: "400px", md: "unset"},
+          padding: {xs: "1rem", sm: "2rem", md: "2rem"},
+        }}
+      > 
+      <Paper
+      elevation={3}
+      sx={{
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "162px",
+        aspectRatio: "1/1",
+        borderRadius: "50%",
+        border: service.isFavourite ? `4px solid ${palette.primary[400]}` : "none",
+        backgroundColor: "#fff",
+        // boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      }}
+      >
+        {service.isFavourite && <Box component={"img"} src={favourite} alt={service.title} width={"42px"} height={"42px"} loading="lazy" decoding="async" 
+          sx={{position: "absolute", top: "4px", right: "4px",
+            animation: `${rotateScaleUp} 4s linear infinite`,
+          }}
+          />}
+        <Box 
+          component={"img"} 
+          src={service.icon} 
+          alt={service.title} 
+          width={"96px"} 
+          height={"96px"} 
+          loading="lazy"
+          decoding="async"
+        />
+      </Paper>
+      <Box>
+        <Title1 sx={{ textAlign: "center", mb: "1rem", textWrap: "wrap", textTransform: "capitalize" }}>
+          {service.title}
+        </Title1>
+        <Text1 sx={{ textAlign: "center", textWrap: "wrap" }}>
+          {service.description}
+        </Text1>
+      </Box>
+    </Box>
+  )
+}
 export const Services: React.FC = () => {
   const theme = useTheme();
   const { palette } = theme;
@@ -64,73 +141,43 @@ export const Services: React.FC = () => {
       }}
     >
       <SectionTitle id="services-title" >
-        SERVICIOS DESTACADOS
+        ¿QUÉ OFRECEMOS?
       </SectionTitle>
       <SectionSubTitle sx={{ marginBottom: {xs:"3rem", xl:"4rem"}}}>
-      Nos especializamos en brindar soluciones industriales de calidad con personal altamente calificado.
+      Nos especializamos en brindar soluciones industriales de calidad, con maquinaria moderna y personal altamente capacitado que garantizan la excelencia en cada proyecto.
       </SectionSubTitle>
       <Box
         sx={{
           display: "flex",
           // gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr 1fr", xl: "1fr 1fr 1fr" },
-          gap: "4rem",
+          gap: {xs: "4rem", sm: "1rem", md: "4rem", xl: "4rem"},
           justifyContent: "center",
           flexWrap: "wrap",
           maxWidth: "1280px", 
         }}
       >
-        {services.map((service, index) => (
-          <Box
-            key={`service-${index}`}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "1rem",
-              width: {xs: "100%", sm: "45%", md: "30%"},
-              maxWidth: {xs: "400px", md: "unset"},
-              // padding: {xs: "1rem", sm: "2rem", md: "2rem"},
-            }}
-          >
-            <Box
-            sx={{
-              position: "relative",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "162px",
-              aspectRatio: "1/1",
-              borderRadius: "50%",
-              border: service.isFavourite ? `2px solid ${palette.primary.main}` : "none",
-              backgroundColor: "#fff",
-            }}
-            >
-              {service.isFavourite && <Box component={"img"} src={favourite} alt={service.title} width={"42px"} height={"42px"} loading="lazy" decoding="async" 
-                sx={{position: "absolute", top: "4px", right: "4px",}}
-                />}
-              <Box 
-                component={"img"} 
-                src={service.icon} 
-                alt={service.title} 
-                width={"96px"} 
-                height={"96px"} 
-                loading="lazy"
-                decoding="async"
-              />
-            </Box>
-            <Box>
-              <Title1 sx={{ textAlign: "center", mb: "1rem", textWrap: "wrap", textTransform: "capitalize" }}>
-                {service.title}
-              </Title1>
-              <Text1 sx={{ textAlign: "center", textWrap: "wrap" }}>
-                {service.description}
-              </Text1>
-            </Box>
-          </Box>
-          ))
-        }
+        {services.filter((service) => service.isFavourite === false).map((service, index) => (
+          <ServiceCard service={service} key={`service-${index}`} />
+        ))}
       </Box>
+      <SectionTitle id="services-title" >
+      ¿EN QUÉ NOS DESTACAMOS?
+      </SectionTitle>
+      <Box
+        sx={{
+          display: "flex",
+          // gridTemplateColumns: { xs: "1fr", sm: "1fr 1fr", md: "1fr 1fr 1fr", xl: "1fr 1fr 1fr" },
+          gap: {xs: "4rem", sm: "1rem", md: "4rem", xl: "4rem"},
+          justifyContent: "center",
+          flexWrap: "wrap",
+          maxWidth: "1280px", 
+        }}
+      >
+      {services.filter((service) => service.isFavourite === true).map((service, index) => (
+          <ServiceCard service={service} key={`favourite-service-${index}`} />
+      ))}
+      </Box>
+
       <Box component={"span"} id="procedures" />
     </Box>
   );
