50dd8e566dc77bcb660b804c1e580eb3eeefd9ef
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-06-16 20:13:27 -0300

    md equipment - se actualizan las imgs y se les agrega texto a las que lo necesitaban

--- src/modules/home/components/Equipment.tsx
+++ src/modules/home/components/Equipment.tsx
@@ -1,52 +1,82 @@
 // src/modules/home/components/Equipment.tsx
-import { Box, useTheme } from "@mui/material";
 import React from "react";
-import { Title2 } from "@theme/textStyles";
+import { Box, useTheme } from "@mui/material";
+import { Text3, Title2 } from "@theme/textStyles";
 import { SectionTitle } from "./Styled";
-import tornos from "@img/equipment/torno.png";
-import alesadora from "@img/equipment/alesadora.png";
-import limadoras from "@img/equipment/limadora.png";
-import fresadora from "@img/equipment/fresadora.png";
-import perforadoraPie from "@img/equipment/taladro-pie.png";
-import perforadoraRadial from "@img/equipment/taladro-radial.png";
-import prensaHidraulica from "@img/equipment/prensa-hidraulica.png";
-import cortadoraMetales from "@img/equipment/cortadora-metales.png";
-import cortePlasma from "@img/equipment/corte-plasma.png";
-import soldadora from "@img/equipment/soldadora.png";
+import torno1 from "@img/equipment/12-torno_paralelo-hidrocopiador.webp";
+import torno2 from "@img/equipment/13-torno_paralelo-1000.webp";
+import torno3 from "@img/equipment/14-torno_paralelo-1070.webp";
+import alesadora from "@img/equipment/1-alesadora.webp";
+import limadoras from "@img/equipment/7-limadora.webp";
+import fresadora from "@img/equipment/5-fresadora.webp";
+import cepillo from "@img/equipment/2-cepillo_hidráulico.webp";
+import perforadoraPie from "@img/equipment/9-perforadora_pie.webp";
+import perforadoraRadial from "@img/equipment/8-perforadora_radial.webp";
+import prensaHidraulica from "@img/equipment/10-prensa_hidráulica.webp";
+import cortadoraMetales from "@img/equipment/3-cortadora_metales.webp";
+import cortePlasma from "@img/equipment/4-cortadora_plasma.webp";
+import soldadora from "@img/equipment/11-soldadoras.webp";
+import fundidora from "@img/equipment/6-fundidora.webp";
 import { Paper } from "@mui/material";
 
 export const Equipment: React.FC = () => {
   const { palette } = useTheme();
   const equipment = [
-    {title: "Tornos Paralelos",
-    img: tornos,
+    {title: "Torno Paralelo",
+    description: "con hidrocopiador, diám 400mm, largo 1000mm",
+    img: torno1,
+    },
+    {title: "Torno Paralelo",
+    description: "diám 1000mm, largo 4060mm",
+    img: torno2,
+    },
+    {title: "Torno Paralelo",
+    description: "diám 1070mm, largo 3000mm",
+    img: torno3,
     },
     {title: "Alesadora",
+    description: "diám husillo 70mm",
     img: alesadora,
     },
     {title: "Limadoras",
+    description: "largo carrera 600mm",
     img: limadoras,
     },
     {title: "Fresadora",
+    description: "",
     img: fresadora,
     },
+    {title: "Cepillo Hidráulico",
+    description: "",
+    img: cepillo,
+    },
     {title: "Perforadora Radial",
+    description: "largo bandera 1500mm",
     img: perforadoraRadial,
     },
     {title: "Perforadoras de Pie",
+    description: "",
     img: perforadoraPie,
     },
     {title: "Prensa Hidráulica",
+    description: "",
     img: prensaHidraulica,
     },
     {title: "Cortadora de Metales",
+    description: "",
     img: cortadoraMetales,
     },
     {title: "Corte Plasma",
+    description: "",
     img: cortePlasma,
     },
     {title: "Soldadoras",
+    description: "",
     img: soldadora,
+    },
+    {title: "Fundidora",
+    description: "",
+    img: fundidora,
     },
   ]
   return (
@@ -115,7 +145,15 @@ export const Equipment: React.FC = () => {
                 alt={equip.title}
               />
             </Box>
-            <Box sx={{ width: "100%", height: "33.33%", display: "flex", alignItems: "center", justifyContent: "center", }}>
+            <Box sx={{ 
+              width: "100%", 
+              height: "33.33%", 
+              display: "flex", 
+              flexDirection: "column ", 
+              alignItems: "center", 
+              justifyContent: "center", 
+              gap: {xs:"3px", md:"5px", lg:"7px"} 
+            }}>
               <Title2 sx={{ 
                 textAlign: "center", 
                 color: "text.primary", 
@@ -124,11 +162,24 @@ export const Equipment: React.FC = () => {
                 width: "100%", 
                 background: "background.paper",
                 paddingX: {xs: "1rem", md: "", lg: "0.5rem"},
-                lineHeight: "1.2",
+                lineHeight: "0.9",
                 transform: {xs: "scale(90%)", md: "unset"},
               }}>
                 {equip.title}
               </Title2>
+              <Text3 sx={{ 
+                textAlign: "center", 
+                color: "text.primary", 
+                textWrap: "wrap", 
+                textTransform: "none", 
+                width: "100%", 
+                background: "background.paper",
+                paddingX: {xs: "1rem", md: "", lg: "0.5rem"},
+                lineHeight: "0.9",
+                transform: {xs: "scale(75%)", md: "scale(85%)"},
+              }}>
+              {equip.description}
+              </Text3>
             </Box>
           </Paper>
           ))
